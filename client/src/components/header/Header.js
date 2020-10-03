68c81ef208abce38829ac1c1bf3e54ee3510bcd1
David Guzman <davidguzmaninc@gmail.com>
2020-10-03 00:58:38 -0400

    Added a header an default styling

---
+++ client/src/components/header/Header.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+export const Header = () => {
+  return (
+    <div className='Header'>
+      <h1>The Fruit Basket</h1>
+    </div>
+  );
+};
+
+export default Header;
